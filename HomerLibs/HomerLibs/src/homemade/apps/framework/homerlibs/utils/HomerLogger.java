6fd19ce7c41aa73854e7e48da96dd8949700f515
aman satija <satija.aman@gmail.com>
2013-05-29 19:43:46 +0530

    Get Tags Automatically fixed some bugs ...!!

--- HomerLibs/HomerLibs/src/homemade/apps/framework/homerlibs/utils/HomerLogger.java
+++ HomerLibs/HomerLibs/src/homemade/apps/framework/homerlibs/utils/HomerLogger.java
@@ -1,6 +1,10 @@
 package homemade.apps.framework.homerlibs.utils;
 
+import android.content.Context;
 import android.util.Log;
+/////
+////ARGUABLY THE BEST LOGGER CLASS FOR ANDROID IN THE WHOLE GOD DAMN WORLD ,,,,,,,,!!!!!!!
+/////
 
 public class HomerLogger {
 
@@ -16,8 +20,11 @@ public class HomerLogger {
 
 	// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// //////
-	// //////features to be added later on :: a boolean to stop all logs from
-	// homer libs;
+	// //////features to be added later on ::
+	// a )boolean to stop all logs from homer libs;
+	//
+	// b )ability to generate a log.txt file of all the logs that were Logged
+	// through Hommer Logger
 	// //////
 	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	public HomerLogger() {
@@ -78,7 +85,7 @@ public class HomerLogger {
 		HomerLogger.mKeepLoggingD = mKeepLoggingD;
 	}
 
-	public boolean keepLoggingAtObjectLevel() {
+	public boolean getkeepLoggingAtObjectLevel() {
 		return mkeepLoggingAtObjectLevel;
 	}
 
@@ -91,102 +98,188 @@ public class HomerLogger {
 	// // Logging functions............At global level ...!!!
 	// //
 	// //////////////////////////////////////////////////////////////////////////////////////////////////
+	/**
+	 * ........................................................................
+	 * ...............................
+	 **/
+	public static void e(String msg) {
+
+		HomerLogger.e(null, msg, null);
+
+	}
 
 	public static void e(String debugtab, String msg) {
-		if (keepLogging()) {
-			if (keepLoggingE()) {
-				HomerLogger.e(debugtab, msg, null);
-			}
-		}
+
+		HomerLogger.e(debugtab, msg, null);
+
 	}
 
 	public static void e(String debugtab, String msg, Exception e) {
 		if (keepLogging()) {
 			if (keepLoggingE()) {
 				if (e == null) {
-					Log.e(debugtab, msg);
+					if (debugtab != null) {
+						Log.e(debugtab, msg);
+					} else {
+						Log.e(getTag(), msg);
+					}
 				} else {
-					Log.e(debugtab, msg, e);
+
+					if (debugtab != null) {
+						Log.e(debugtab, msg, e);
+					} else {
+						Log.e(getTag(), msg, e);
+					}
+
 				}
 			}
 		}
 	}
 
+	/**
+	 * ........................................................................
+	 * ...............................
+	 **/
+	public static void i(String msg) {
+
+		HomerLogger.i(null, msg, null);
+
+	}
+
 	public static void i(String debugtab, String msg) {
-		if (keepLogging()) {
-			if (keepLoggingI()) {
-				HomerLogger.i(debugtab, msg, null);
-			}
-		}
+
+		HomerLogger.i(debugtab, msg, null);
+
 	}
 
 	public static void i(String debugtab, String msg, Exception e) {
 		if (keepLogging()) {
 			if (keepLoggingI()) {
 				if (e == null) {
-					Log.i(debugtab, msg);
+					if (debugtab != null) {
+						Log.i(debugtab, msg);
+					} else {
+						Log.i(getTag(), msg);
+					}
 				} else {
-					Log.i(debugtab, msg, e);
+					if (debugtab != null) {
+						Log.i(debugtab, msg, e);
+					} else {
+						Log.i(getTag(), msg, e);
+					}
 				}
 			}
 		}
 	}
 
+	/**
+	 * ........................................................................
+	 * ...............................
+	 **/
+	public static void v(String msg) {
+
+		HomerLogger.v(null, msg, null);
+
+	}
+
 	public static void v(String debugtab, String msg) {
-		if (keepLogging()) {
-			if (keepLoggingV()) {
-				HomerLogger.v(debugtab, msg, null);
-			}
-		}
+
+		HomerLogger.v(debugtab, msg, null);
+
 	}
 
 	public static void v(String debugtab, String msg, Exception e) {
 		if (keepLogging()) {
 			if (keepLoggingV()) {
 				if (e == null) {
-					Log.v(debugtab, msg);
+					if (debugtab != null) {
+						Log.v(debugtab, msg);
+					} else {
+						Log.v(getTag(), msg);
+					}
 				} else {
-					Log.v(debugtab, msg, e);
+					if (debugtab != null) {
+						Log.v(debugtab, msg, e);
+					} else {
+						Log.v(getTag(), msg, e);
+					}
+
 				}
 			}
 		}
 	}
 
+	/**
+	 * ........................................................................
+	 * ...............................
+	 **/
+	public static void w(String msg) {
+
+		HomerLogger.w(null, msg, null);
+
+	}
+
 	public static void w(String debugtab, String msg) {
-		if (keepLogging()) {
-			if (keepLoggingW()) {
-				HomerLogger.w(debugtab, msg, null);
-			}
-		}
+
+		HomerLogger.w(debugtab, msg, null);
+
 	}
 
 	public static void w(String debugtab, String msg, Exception e) {
 		if (keepLogging()) {
 			if (keepLoggingW()) {
 				if (e == null) {
-					Log.w(debugtab, msg);
+					if (debugtab != null) {
+						Log.w(debugtab, msg);
+					} else {
+						Log.w(getTag(), msg);
+					}
 				} else {
-					Log.w(debugtab, msg, e);
+					if (debugtab != null) {
+						Log.w(debugtab, msg, e);
+					} else {
+						Log.w(getTag(), msg, e);
+					}
+
 				}
 			}
 		}
 	}
 
+	/**
+	 * ........................................................................
+	 * ...............................
+	 **/
+	public static void d(String msg) {
+
+		HomerLogger.d(null, msg, null);
+
+	}
+
 	public static void d(String debugtab, String msg) {
-		if (keepLogging()) {
-			if (keepLoggingD()) {
-				HomerLogger.e(debugtab, msg, null);
-			}
-		}
+
+		HomerLogger.d(debugtab, msg, null);
+
 	}
 
 	public static void d(String debugtab, String msg, Exception e) {
 		if (keepLogging()) {
 			if (keepLoggingD()) {
 				if (e == null) {
-					Log.d(debugtab, msg);
+					if (debugtab != null) {
+						Log.d(debugtab, msg);
+					} else {
+						Log.d(getTag(), msg);
+					}
 				} else {
-					Log.d(debugtab, msg, e);
+
+					if (debugtab != null) {
+						Log.d(debugtab, msg, e);
+
+					} else {
+						Log.d(getTag(), msg, e);
+
+					}
 				}
 			}
 		}
@@ -197,140 +290,183 @@ public class HomerLogger {
 	// // Logging functions............At Object level ...!!!
 	// //
 	// //////////////////////////////////////////////////////////////////////////////////////////////////
+	/**
+	 * ........................................................................
+	 * ...............................
+	 **/
+	public void eAtObjectLevel(String msg) {
+		if (getkeepLoggingAtObjectLevel()) {
+
+			HomerLogger.e(null, msg, null);
+
+		}
+
+	}
 
 	public void eAtObjectLevel(String debugtab, String msg) {
-		if (keepLoggingAtObjectLevel()) {
-			if (keepLogging()) {
-				if (keepLoggingE()) {
-					HomerLogger.e(debugtab, msg, null);
-				}
-			}
+		if (getkeepLoggingAtObjectLevel()) {
+
+			HomerLogger.e(debugtab, msg, null);
+
 		}
 
 	}
 
 	public void eAtObjectLevel(String debugtab, String msg, Exception e) {
-		if (keepLoggingAtObjectLevel()) {
-			if (keepLogging()) {
-				if (keepLoggingE()) {
-					if (e == null) {
-						Log.e(debugtab, msg);
-					} else {
-						Log.e(debugtab, msg, e);
-					}
-				}
-			}
+		if (getkeepLoggingAtObjectLevel()) {
+			HomerLogger.e(debugtab, msg, e);
+		}
+
+	}
+
+	/**
+	 * ........................................................................
+	 * ...............................
+	 **/
+	public void iAtObjectLevel(String msg) {
+		if (getkeepLoggingAtObjectLevel()) {
+
+			HomerLogger.i(null, msg, null);
+
 		}
 
 	}
 
 	public void iAtObjectLevel(String debugtab, String msg) {
-		if (keepLoggingAtObjectLevel()) {
-			if (keepLogging()) {
-				if (keepLoggingI()) {
-					HomerLogger.i(debugtab, msg, null);
-				}
-			}
+		if (getkeepLoggingAtObjectLevel()) {
+
+			HomerLogger.i(debugtab, msg, null);
+
 		}
 
 	}
 
 	public void iAtObjectLevel(String debugtab, String msg, Exception e) {
-		if (keepLoggingAtObjectLevel()) {
-			if (keepLogging()) {
-				if (keepLoggingI()) {
-					if (e == null) {
-						Log.i(debugtab, msg);
-					} else {
-						Log.i(debugtab, msg, e);
-					}
-				}
-			}
+		if (getkeepLoggingAtObjectLevel()) {
+			HomerLogger.i(debugtab, msg, e);
+		}
+
+	}
+
+	/**
+	 * ........................................................................
+	 * ...............................
+	 **/
+	public void vAtObjectLevel(String msg) {
+		if (getkeepLoggingAtObjectLevel()) {
+
+			HomerLogger.v(null, msg, null);
+
 		}
 
 	}
 
 	public void vAtObjectLevel(String debugtab, String msg) {
-		if (keepLoggingAtObjectLevel()) {
-			if (keepLogging()) {
-				if (keepLoggingV()) {
-					HomerLogger.v(debugtab, msg, null);
-				}
-			}
+		if (getkeepLoggingAtObjectLevel()) {
+
+			HomerLogger.v(debugtab, msg, null);
+
 		}
 
 	}
 
 	public void vAtObjectLevel(String debugtab, String msg, Exception e) {
-		if (keepLoggingAtObjectLevel()) {
-			if (keepLogging()) {
-				if (keepLoggingV()) {
-					if (e == null) {
-						Log.v(debugtab, msg);
-					} else {
-						Log.v(debugtab, msg, e);
-					}
-				}
-			}
+		if (getkeepLoggingAtObjectLevel()) {
+			HomerLogger.v(debugtab, msg, e);
+		}
+
+	}
+
+	/**
+	 * ........................................................................
+	 * ...............................
+	 **/
+	public void wAtObjectLevel(String msg) {
+		if (getkeepLoggingAtObjectLevel()) {
+
+			HomerLogger.w(null, msg, null);
+
 		}
 
 	}
 
 	public void wAtObjectLevel(String debugtab, String msg) {
-		if (keepLoggingAtObjectLevel()) {
-			if (keepLogging()) {
-				if (keepLoggingW()) {
-					HomerLogger.w(debugtab, msg, null);
-				}
-			}
+		if (getkeepLoggingAtObjectLevel()) {
+
+			HomerLogger.w(debugtab, msg, null);
+
 		}
 
 	}
 
 	public void wAtObjectLevel(String debugtab, String msg, Exception e) {
-		if (keepLoggingAtObjectLevel()) {
-			if (keepLogging()) {
-				if (keepLoggingW()) {
-					if (e == null) {
-						Log.w(debugtab, msg);
-					} else {
-						Log.w(debugtab, msg, e);
-					}
-				}
-			}
+		if (getkeepLoggingAtObjectLevel()) {
+			HomerLogger.w(debugtab, msg, e);
+		}
+
+	}
+
+	/**
+	 * ........................................................................
+	 * ...............................
+	 **/
+	public void dAtObjectLevel(String msg) {
+		if (getkeepLoggingAtObjectLevel()) {
+
+			HomerLogger.d(null, msg, null);
+
 		}
 
 	}
 
 	public void dAtObjectLevel(String debugtab, String msg) {
-		if (keepLoggingAtObjectLevel()) {
-			if (keepLogging()) {
-				if (keepLoggingD()) {
-					HomerLogger.e(debugtab, msg, null);
-				}
-			}
+		if (getkeepLoggingAtObjectLevel()) {
+
+			HomerLogger.d(debugtab, msg, null);
+
 		}
 
 	}
 
 	public void dAtObjectLevel(String debugtab, String msg, Exception e) {
-		if (keepLoggingAtObjectLevel()) {
-			if (keepLogging()) {
-				if (keepLoggingD()) {
-					if (e == null) {
-						Log.d(debugtab, msg);
-					} else {
-						Log.d(debugtab, msg, e);
-					}
-				}
-			}
+		if (getkeepLoggingAtObjectLevel()) {
+			HomerLogger.d(debugtab, msg, e);
 		}
 
 	}
+
 	// ////////////////////////////////////////////////////////////////////////////////////////////////////
 	// //
 	// //a function for logging that onli hommer libs uses ,,,!! so that we can
 	// stop all logs from homer libs by just one boolean
 	// //
 	// //////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * ........................................................................
+	 * ...............................
+	 **/
+
+	// ////////////////////////////////////////////////////////////////////////////////////////////////////
+	// //
+	// //Below is a function that automatically gets the tag for the logging
+	// functions the string returned by this function would something like this
+	// "ClassName.MehtodName.LineNo"
+	// //
+	// //////////////////////////////////////////////////////////////////////////////////////////////////
+	private static String getTag() {
+
+		StackTraceElement caller = Thread.currentThread().getStackTrace()[5];
+		String c = caller.getClassName();
+		String className = c.substring(c.lastIndexOf(".") + 1, c.length());
+		StringBuilder sb = new StringBuilder(5);
+		sb.append(className);
+		sb.append(".");
+		sb.append(caller.getMethodName());
+		sb.append("():");
+		sb.append(caller.getLineNumber());
+		return sb.toString();
+
+	}
+
 }
