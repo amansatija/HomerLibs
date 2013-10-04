5953047772a1be4bee0993c7df1b25843c1da07b
vivek shukla <vivek_shukla@techanveshan.com>
2013-10-04 20:51:21 +0530

    deleting old version of homerlibs

--- HomerLibs/src/homemade/apps/framework/homerlibs/activities/ActivityHomerBase.java
+++
@@ -1,9 +0,0 @@
-package homemade.apps.framework.homerlibs.activities;
-
-import android.app.Activity;
-
-public class ActivityHomerBase extends Activity {
-
-	
-	
-}
--- HomerLibs/src/homemade/apps/framework/homerlibs/utils/network/HttpData.java
+++
@@ -1,7 +0,0 @@
-package homemade.apps.framework.homerlibs.utils.network;
-import java.util.Hashtable;
-public class HttpData {
-      public String content;
-      public Hashtable cookies = new Hashtable();
-      public Hashtable headers = new Hashtable();
-}
